c3c0fc20cd1a776155b645fdd553aec43fcf659e
medoussboug <mohamedoussama.bougueddach@gmail.com>
2022-01-03 11:43:07 +0100

    Create table to store different cryptocurrencies

---
+++ src/main/java/com/example/cryptotracker/domain/Cryptocurrency.java
@@ -0,0 +1,66 @@
+package com.example.cryptotracker.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+@Builder
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cryptocurrency {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "symbol")
+    private String symbol;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "image")
+    private String image;
+    @Column(name = "current_price")
+    private Double currentPrice;
+    @Column(name = "market_cap")
+    private Long marketCap;
+    @Column(name = "market_cap_rank")
+    private Long marketCapRank;
+    @Column(name = "fully_diluted_valuation")
+    private Long fullyDilutedValuation;
+    @Column(name = "total_volume")
+    private Long totalVolume;
+    @Column(name = "high_24h")
+    private Double high24h;
+    @Column(name = "low_24h")
+    private Double low24h;
+    @Column(name = "price_change_24h")
+    private Double priceChange24h;
+    @Column(name = "price_change_percentage_24h")
+    private Double priceChangePercentage24h;
+    @Column(name = "market_cap_change_24h")
+    private Double marketCapChange24h;
+    @Column(name = "market_cap_change_percentage_24h")
+    private Double marketCapChangePercentage24h;
+    @Column(name = "circulating_supply")
+    private Double circulatingSupply;
+    @Column(name = "total_supply")
+    private Double totalSupply;
+    @Column(name = "max_supply")
+    private Double maxSupply;
+    @Column(name = "ath")
+    private Double ath;
+    @Column(name = "ath_change_percentage")
+    private Double athChangePercentage;
+    @Column(name = "ath_date")
+    private String athDate;
+    @Column(name = "atl")
+    private Double atl;
+    @Column(name = "atl_change_percentage")
+    private Double atlChangePercentage;
+    @Column(name = "atl_date")
+    private String atlDate;
+    @Column(name = "last_updated")
+    private String lastUpdated;
+}
