c629958fca288c0398dd3a001df4da15e0db08d2
medoussboug <mohamedoussama.bougueddach@gmail.com>
2022-01-06 15:10:11 +0100

    Refactor the loading message to notify users if the price has gone to there desired price

--- src/main/java/com/example/cryptotracker/domain/CryptocurrencyService.java
+++ src/main/java/com/example/cryptotracker/domain/CryptocurrencyService.java
@@ -5,10 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
 public class CryptocurrencyService {
+    @Autowired
+    private final SmsSender smsSender;
+
     @Autowired
     private final CryptocurrencyMapper cryptocurrencyMapper;
 
@@ -16,12 +21,37 @@ public class CryptocurrencyService {
     private final CryptocurrencyRepository cryptocurrencyRepository;
 
     @Autowired
+    private final FavoriteCryptocurrencyRepository favoriteCryptocurrencyRepository;
+
+    @Autowired
     private final CoinGeckoClient coinGeckoClient;
 
     public void loadData() {
+        System.out.println("bsmlah");
         List<CryptocurrencyDTO> loadedCryptocurrencies = coinGeckoClient.getCryptocurrencies();
         for (CryptocurrencyDTO loadedCryptocurrency : loadedCryptocurrencies) {
+            Optional<Cryptocurrency> cryptocurrency = cryptocurrencyRepository.findById(loadedCryptocurrency.id);
+            if (cryptocurrency.isPresent() && !loadedCryptocurrency.currentPrice.equals(cryptocurrency.get().getCurrentPrice())) {
+                Set<FavoriteCryptocurrency> favoriteCryptocurrencies = cryptocurrency.get().getUsersFavoriteCryptocurrencies();
+                if (favoriteCryptocurrencies.size() > 0) {
+                    for (FavoriteCryptocurrency favoriteCryptocurrency : favoriteCryptocurrencies) {
+                        if (loadedCryptocurrency.currentPrice <= favoriteCryptocurrency.getDesiredBuyingPrice() || loadedCryptocurrency.currentPrice >= favoriteCryptocurrency.getDesiredSellingPrice()) {
+                            smsSender.sendSms(
+                                    new SmsRequest(
+                                            favoriteCryptocurrency.getUser()
+                                                    .getPhoneNumber(),
+                                            String.format("3lach nta zaml", favoriteCryptocurrency.getCryptoName())
+                                    )
+                            );
+                        }
+                        favoriteCryptocurrency.setCryptoPrice(loadedCryptocurrency.currentPrice);
+                        favoriteCryptocurrencyRepository.save(favoriteCryptocurrency);
+                    }
+                }
+
+            }
             cryptocurrencyRepository.save(cryptocurrencyMapper.mapToEntity(loadedCryptocurrency));
+
         }
     }
 
